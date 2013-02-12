efe9699dbff4ceb3a2a68b8c7c9d0a918f1d6304
Kai Armstrong <kai.armstrong@gmail.com>
2013-02-12 14:17:58 -0600

    added tags to posts

--- runkeeper-wordpress-activity-feed.php
+++ runkeeper-wordpress-activity-feed.php
@@ -180,7 +180,8 @@ function toz_rk_schedule_event() {
 							'post_date'     => date_format($publish_date, 'Y-m-d H:i:s'), //this is converted activity date
 							'post_status'   => 'publish',
 							'post_author'   => get_option('toz_rk_author_id'),
-							'post_category' => array(get_option('toz_rk_post_categories'))
+							'post_category' => array(get_option('toz_rk_post_categories')),
+							'tags_input'    => $rkActivity_detailed_array['type']
 							);
 						$post_id = wp_insert_post( $toz_rk_post_import );
 					
@@ -244,6 +245,7 @@ function toz_rk_import_old() {
 						'post_status'   => 'publish',
 						'post_author'   => get_option('toz_rk_author_id'),
 						'post_category' => array(get_option('toz_rk_post_categories'))
+						'tags_input'    => $rkActivity_detailed_array['type']
 					);
 					$post_id = wp_insert_post( $toz_rk_post_import );
 					
