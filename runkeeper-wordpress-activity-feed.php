27db3ec4b509670c94d5a686edb6cd44366fc8e2
Kai Armstrong <kai.armstrong@gmail.com>
2013-02-19 22:42:50 -0600

    version increase for new feature

--- runkeeper-wordpress-activity-feed.php
+++ runkeeper-wordpress-activity-feed.php
@@ -5,7 +5,7 @@ Plugin Name: RunKeeper WordPress Activity Feed
 Plugin URI: http://runkeeper.thinkonezero.com
 Description: A plugin to automatically draft posts of all your Runkeeper Activities.
 Author: A. Kai Armstrong
-Version: 1.0.7
+Version: 1.1.0
 Author URI: http://www.kaiarmstrong.com
 */
 
