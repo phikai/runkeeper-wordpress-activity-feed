abe5cfe9824dca98765e0aaf09a76f8c3f0864fc
Kai Armstrong <kai.armstrong@gmail.com>
2013-02-23 22:48:19 -0600

    correction to deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
-SVNURL="http://plugins.svn.wordpress.org/______your-plugin-name______/" # Remote SVN repo on wordpress.org, with no trailing slash
+SVNURL="http://plugins.svn.wordpress.org/runkeeper-activity-feed/" # Remote SVN repo on wordpress.org, with no trailing slash
 SVNUSER="phikai" # your svn username
 
 
